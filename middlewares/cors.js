b64155e8a8c512213038ccc59bc1d2595563e90f
Cl0uDDy <149698532+Cl0uDDy@users.noreply.github.com>
2024-05-31 21:58:14 +0700

    new

---
+++ middlewares/cors.js
@@ -0,0 +1,21 @@
+const allowedCors = [
+  "https://pindie-frontend22.nomoredomainswork.ru",
+  "http://pindie-frontend22.nomoredomainswork.ru",
+  "https://localhost:3000"
+];
+
+function cors(req, res, next) {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
+  next();
+}
+
+module.exports = cors;
